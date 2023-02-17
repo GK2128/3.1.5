331ee3175cd31617ca1b921659d4521505f217fd
GK2128 <puyol2128@mail.ru>
2023-02-17 12:34:19 +0300

    2.0

--- src/main/java/ru/kata/spring/boot_security/demo/controllers/UserController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/UserController.java
@@ -9,15 +9,9 @@ import ru.kata.spring.boot_security.demo.service.UserService;
 
 @Controller
 public class UserController {
-	private final UserService userService;
-	public UserController(UserService userService) {
-		this.userService = userService;
-	}
 
 	@GetMapping(value = "/user")
-	public String userPage(Model model, @AuthenticationPrincipal User user) {
-		model.addAttribute("user", user);
-
+	public String userPage() {
 		return "user";
 	}
 
